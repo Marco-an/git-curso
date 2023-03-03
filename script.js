06eba78667a25877981e77052ace27239540f961
menor <>
2023-03-02 23:00:39 -0500

    implementacion de function saludar

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 for (let i = 0; i < 10; i++) {
   console.log(i);
 }
+function saludar(name, age) {
+  return `Hola ${name} su edad es ${age}`;
+}
+let userDates = saludar('Marco', 18);
+console.log(saludar());
